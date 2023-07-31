044c8e0b924246c442770a2e1be248091f76a7fa
Samuel Kyletoft <samuel@kyletoft.se>
2023-07-31 21:22:55 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct Ground;
+
+fn main() {
+	App::new()
+		.add_plugins(DefaultPlugins)
+		.add_systems(
+			Startup,
+			setup,
+		)
+		.add_systems(Update, update)
+		.run();
+}
+
+pub fn update(
+	mut query: Query<&mut Transform, With<Ground>>,
+	d_time: Res<Time>,
+) {
+	for mut t in query.iter_mut() {
+		t.translation.z = d_time.elapsed_seconds().sin();
+		t.rotation.y = (d_time.elapsed_seconds() / 1.2).sin() * 0.4;
+	}
+}
+
+pub fn setup(
+	mut commands: Commands,
+	asset_server: Res<AssetServer>,
+	mut meshes: ResMut<Assets<Mesh>>,
+	mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+	let half_scale = Vec3 {
+		x: 2.0,
+		y: 1.0,
+		z: 2.0,
+	};
+
+	let ground: Handle<Mesh> = asset_server.load("SimpleGround.gltf#Mesh0/Primitive0");
+	let depth: Handle<Image> = asset_server.load("EasyGroundDepth.png");
+	let albedo: Handle<Image> = asset_server.load("EasyGroundAlbedo.png");
+	let overlay: Handle<Image> = asset_server.load("EasyGroundOverlay.png");
+	let normal: Handle<Image> = asset_server.load("EasyGroundNormal.png");
+
+	commands.spawn((PbrBundle {
+		mesh: ground.clone(),
+		material: materials.add(StandardMaterial {
+			base_color_texture: Some(albedo),
+			normal_map_texture: Some(normal),
+			depth_map: Some(depth),
+			..default()
+		}),
+		transform: Transform::from_xyz(0.0, 0.01, 0.0).with_scale(half_scale),
+		..default()
+	}, Ground));
+
+	commands.spawn(DirectionalLightBundle {
+		directional_light: DirectionalLight {
+			shadows_enabled: true,
+			illuminance: 25_000.0,
+			..default()
+		},
+		transform: Transform::from_xyz(4.0, 8.0, 14.0).looking_at(Vec3::ZERO, Vec3::Y),
+		..default()
+	});
+
+	commands.spawn(Camera3dBundle {
+		transform: Transform::from_xyz(-30.0, 25.0, 2.0).looking_at(Vec3::ZERO, Vec3::Y),
+		..default()
+	});
+}
